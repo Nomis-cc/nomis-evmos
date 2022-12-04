869653a7aa89e8b8ce0238def910301d84e372f8
Chebotov Nickolay <spiritkola@hotmail.com>
2022-12-05 00:07:47 +0300

    Initial commit

---
+++ web/utilities/blockchains.js
@@ -0,0 +1,22 @@
+export const blockchains = [
+  {
+    item: "Evmos",
+    slug: "evmos",
+    icon: "/blockchains/evmos.svg",
+    placeholder: "0x... address",
+    coin: "EVMOS",
+    contractAddress: "0x361534d5ee6a1ce28add20905867eea4d02a0b04",
+    chainId: 9001,
+    networkData: {
+      chainId: "0x2329",
+      chainName: "Evmos",
+      nativeCurrency: {
+        name: "EVMOS",
+        symbol: "EVMOS",
+        decimals: 18
+      },
+      rpcUrls: ["https://eth.bd.evmos.org:8545"],
+      blockExplorerUrls: ["https://evm.evmos.org"]
+    },
+  },
+];
